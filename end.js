d483d175135e9933e829bdfccd80d2fab21c7968
kimkesdev <Kimboelens@gmail.com>
2021-03-09 09:45:43 +0000

    load and display highscores

--- end.js
+++ end.js
@@ -1,8 +1,13 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
-const mostRecentSore = localStorage.getItem('mostRecentScore');
-finalScore.innerText = mostRecentSore;
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const saveHighScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
@@ -11,4 +16,16 @@ username.addEventListener('keyup', () => {
 saveHighScore = (e) => {
     console.log("clicked the sae button!")
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name; username.value
+    };
+    highScores.push(score);
+    highScores.sort( (a,b) => b.score - a.score)
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign(*/*);
+
 };
